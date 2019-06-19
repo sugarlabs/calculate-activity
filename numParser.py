9a28df54d0d1aaff8ffb0b6a4d83623a1b488f32
nswarup14 <nswarup14@gmail.com>
2019-06-19 17:40:42 +0530

    Replace numerals for Malayalam language with the right one

--- numParser.py
+++ numParser.py
@@ -9,7 +9,7 @@ indic = {u'۰': u'0', u'۱': u'1', u'۲': u'2', u'۳': u'3', u'۴': u'4', u'۵':
 tamil = {u'௦': u'0', u'௧': u'1', u'௨': u'2', u'௩': u'3', u'௪': u'4', u'௫': u'5', u'௬': u'6', u'௭': u'7', u'௮': u'8', u'௯': u'9'}
 devanagari = {u'०': u'0', u'१': u'1', u'२': u'2', u'३': u'3', u'४': u'4', u'५': u'5', u'६': u'6', u'७': u'7', u'८': u'8', u'९': u'9'}
 kannada = {u'0': u'0', u'౧': u'1', u'౨': u'2', u'౩': u'3', u'౪': u'4', u'౫': u'5', u'౬': u'6', u'౭': u'7', u'౮': u'8', u'౯': u'9'}
-malayam = {u'൦': u'0', u'൧': u'1', u'൨': u'2', u'൩': u'3', u'൪': u'4', u'൫': u'5', u'൬': u'6', u'൬': u'7', u'൮': u'8', u'൯': u'9'}
+malayam = {u'൦': u'0', u'൧': u'1', u'൨': u'2', u'൩': u'3', u'൪': u'4', u'൫': u'5', u'൬': u'6', u'൭': u'7', u'൮': u'8', u'൯': u'9'}
 
 if locale in ['ar_SA.utf8', 'ar_YE.utf8', 'ar_AE.utf8', 'ar_SY.utf8', 'ar_OM.utf8', 'ar_JO.utf8', 'ar_IQ.utf8', 'ar_KW.utf8', 'ar_LB.utf8', 'ar_IQ.utf8', 'ar_SD.utf8', 'ar_EG.utf8', 'fa_IR.utf8']:
     localDic = indic
